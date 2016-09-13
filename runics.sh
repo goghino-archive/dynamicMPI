860a3fa5631eb7cf6504d410890871d5d9f69995
Juraj Kardos <juraj.kardos@usi.ch>
2016-09-13 14:06:44 +0200

    working srun (openMPI with PMI support)

--- runics.sh
+++ runics.sh
@@ -11,8 +11,15 @@ sbatch <<-_EOF
 #SBATCH --time=00:02:00
 #SBATCH --output=out.o
 
-#srun -n 1 --ntasks-per-node 1 ./manager ${nworkers}
+#working only with openmpi build with PMI support
+srun -n 1 ./manager ${nworkers}
+
+#error on mpi init
 #./manager ${nworkers}
+
+#timeout
 #/home/kardos/openmpi-2.0.1/bin/mpirun -np 1 ./manager ${nworkers}
-mpirun -np 1 ./manager ${nworkers}
+
+#error on MPI_comm_spawn
+#mpirun -np 1 ./manager ${nworkers}
 _EOF
